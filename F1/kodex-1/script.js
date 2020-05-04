e183e1cb8ce6d7e351e4a0bb6a1db9a81399fc9a
aleksandarmlinar <60160624+aleksandarmlinar@users.noreply.github.com>
2020-05-04 16:04:25 +0200

    mlk

--- F1/kodex-1/script.js
+++ F1/kodex-1/script.js
@@ -3,54 +3,33 @@ window.addEventListener('load', myFunction);
 
 
 // https://restcountries.eu/rest/v2/name/sweden
+
 function myFunction(){
 
-    let container = document.getElementById('content');
-    let preloader = document.querySelector('#preloader');
-
-
-
-
-    window.fetch('https://restcountries.eu/rest/v2/name/sweden').then(function(response){
-        return response.json();
-    }).then(function(data){
-
-        let countryData = data[0];
-        console.log(countryData);        
-
-
-        let card = document.createElement('div');
-        card.className = 'card';
-        card.style.maxWidth = '20rem';
-        container.appendChild(card);
     
-        let cardImage = document.createElement("img");
-        cardImage.className = 'card-img-top';
-        cardImage.src = countryData.flag;
-        card.appendChild(cardImage);
+    let container = document.querySelector('#content');
+    let preloader = document.querySelector('#preloader');
+    
+    
+    window.fetch('https://restcountries.eu/rest/v2/name/sweden')
+        .then(function(response){
+            return response.json();
+        }).then(function(data){
+            //console.log(data);
+            let countryData = data[0];
 
-        let cardBody = document.createElement('div');
-        cardBody.className = 'card-body';
-        card.appendChild(cardBody);
-
-        let cardTitle = document.createElement('h5');
-        cardTitle.className = 'card-title';
-        cardTitle.textContent = countryData.name;
-        cardBody.appendChild(cardTitle);
-
-        let capital = document.createElement('p');
-        capital.className = 'card-text';
-        capital.innerHTML = '<b>Capital: </b>' + countryData.capital;
-        cardBody.appendChild(capital);
-
-        let area = document.createElement('p');
-        area.classList.add('card-text');
-        area.innerHTML = '<b>Area: </b>' + countryData.area.toLocaleString() + 'km<sup>2</sup>';
-        cardBody.appendChild(area);
-
-        preloader.classList.add('d-none');
+            let card = document.createElement('div');
+            card.className = 'card';
+            card.style.maxWidth = '20rem';
+            container.appendChild(card);
 
 
-    });
+            let cardImage = document.createElement('img');
+            cardImage.className = 'card-img-top';
+            cardImage.src = countryData.flag;
+            card.appendChild(cardImage);
+            
+        });
 
-}
+
+};   
