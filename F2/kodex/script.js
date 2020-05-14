88194264d2451d6f40dcbdaf770c60907fdc4740
aleksandarmlinar <60160624+aleksandarmlinar@users.noreply.github.com>
2020-05-14 20:25:17 +0200

    Update script.js

--- F2/kodex/script.js
+++ F2/kodex/script.js
@@ -1,23 +1,19 @@
-/**
- * Makes a request to https://restcountries.eu/rest/v2/name/{query} and displays a table with the results.
- * @param {string} query The user’s search query
- * @param {HTMLElement} container The <tbody> element that the result will be printed to
- */
+'use strict';
 
- window.addEventListener('load', init);
+window.addEventListener('load', init);
 
- function init() {
-  
-  let form = document.querySelector('#search-form');
-  form.addEventListener('submit', handleSearchFormSubmit);
- }
 
- function handleSearchFormSubmit(evt) {
-   evt.preventDefault();
-   let searchField = document.querySelector('#search');
-   let tbody = document.querySelector('#tbody');   
-   search(searchField.value, tbody);
- }
+function init(){
+    let form = document.querySelector('#search'); 
+    form.addEventListener('submit', handleSearchFormSubmit()); 
+};
+
+
+function handleSearchFormSubmit(evt){
+    evt.preventDefault
+};
+
+
 
 
 
@@ -40,36 +36,3 @@ function search(query, container) {
 
 }
 
-function createTableRow(countryData, container) {
-  let row = document.createElement('tr');
-  container.appendChild(row);
-
-  let name = document.createElement('td');
-  name.textContent = countryData.name;
-  row.appendChild(name);
-
-  let capital = document.createElement('td');
-  capital.textContent = countryData.capital;
-  row.appendChild(capital);
-
-  let area = document.createElement('td');
-  area.className = 'text-right';
-  if(countryData.area != null) {
-    area.innerHTML = countryData.area.toLocaleString('en') + ' km<sup>2</sup>';
-  }
-  else {
-    area.textContent = 'n/a';
-  }
-  row.appendChild(area);
-
-  let population = document.createElement('td');
-  population.className = 'text-right';
-  if(countryData.population != null) {
-    population.innerHTML = countryData.population.toLocaleString('en');
-  }
-  else {
-    population.textContent = 'n/a';
-  }
-  row.appendChild(population);
-
-}
