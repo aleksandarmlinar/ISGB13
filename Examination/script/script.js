722720500decdbc052078f8736fd534bde5aaf7a
aleksandarmlinar <60160624+aleksandarmlinar@users.noreply.github.com>
2020-05-29 06:49:23 +0200

    Update script.js

--- Examination/script/script.js
+++ Examination/script/script.js
@@ -24,7 +24,7 @@ function search(query, container){
     container.innerHTML = '';
 
 	//spotify api //
-    var accessToken = 'BQDqUVPNDXQfZ6xTspo54pclqTQifXNzzGMsehpKu0DQWeKrRPZY3NhmkPKK1xAZh7DcJyqO-edes8RKI9GYFKdrbYw12RxyJVoVt5qMQHg3siB9DZHnWz4O5mhhH_tS15eZN4DBy9RmQAps28091fSwaChphrHuFp9eIf898hHTYz5b_7l4gSbtkezptpK3iv1uuPrVw5ViMM1uzT5XlqxwT-rvRWIhPA_MqBIZudxEoR5abcKGbe9nKJmc2UaEgmqpCEBNxRW8ti42Ed28UG4y';    
+    var accessToken = 'BQB5hBpygPmqX0OMavWNM-zWGR3rUBYhEB1_9UtJJtruPboSCkub-Afijqi80a4BXY_H5JVdvqdn0Y5T8GX1cXFzfAA3g6EdlQP1pwfZhLn7roeUKOw2Tng3rAGJuVgi1IEbIIrRoOmWIgRCCyaowMwdN2Cuxn3YQp9QWnBg50ZYh4vJFWL54-_04TmldshqsSdkoCyp4Buysjt51KK8huQEFqe6A0NisWXQaZxU2_O-Z1PgfUmi_5t1KIzwK9rHtzr791HmaIX85r0q2Zh1fIvy';    
     window.fetch('https://api.spotify.com/v1/search?limit=50&q=' + encodeURIComponent(query) + '&type=track', {
             headers: {
                 'Accept': 'application/json',
@@ -47,43 +47,82 @@ function search(query, container){
         };
     });
 
+	//lyrics api
+	//http://api.chartlyrics.com/apiv1.asmx/SearchLyric?artist=string&song=string
+	window.fetch('https://api.spotify.com/v1/search?limit=50&q=' + encodeURIComponent(query) + '&type=track', {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + accessToken
+            }
+        })
+    .then((response)=>{
+        return response.json();
+    })
+    .then((data)=>{
+        //console.log(data.tracks.items[1].artists[0].name);
+        
+        let dataRef = Object.values(data);
+        for(let musicData of dataRef){
+            console.log(musicData);
+            console.log(musicData.items[0].name);   
+            createGridCell(musicData, container);
+       
+        };
+
 };
 
     function createGridCell(musicData, container){
 		
 		document.querySelector('.wrapper').style.display = 'grid';
 		document.querySelector('.content').style.display = 'none';
+		document.querySelector('#closeBtn').style.display = 'none';
+		
 		var closeBtn = document.querySelector('#closeBtn');
 
         for(let i= 0; i < musicData.items.length; i++){
             
+			// Skapa en ruta som vi sen fyller med info från sökfältet.
 			let cellRef = document.createElement('div');
             let cellRefAttr = document.createAttribute('class');
-           
             cellRefAttr.value = 'cell';
             cellRef.setAttributeNode(cellRefAttr);
             cellRef.setAttribute('style', 'background-image: url(' + '"' + musicData.items[i].album.images[0].url + '"); background-size: cover;');
             
             let artistRef = document.createElement('h2');
             let nameRef = document.createElement('h5');
+			let previewRef = document.createElement('div');
 
 
             let songName = musicData.items[i].name;
             let artistName = musicData.items[i].artists[0].name;
+			let previewUrl = musicData.items[i].preview_url;
+			let imageCover = musicData.items[i].album.images[1].url;
+
+			let infoData = {
+
+				songname: songName,
+				artistname: artistName,
+				previewurl: previewUrl,
+				imagecover: imageCover
+			};
 
 
-            nameRef.innerHTML= songName;
-            artistRef.innerHTML = artistName;
+            nameRef.textContent = songName;
+            artistRef.textContent = artistName;
+			previewRef.innerHTML = "<iframe src=" + previewUrl + "></iframe>";
+			
 
             container.appendChild(cellRef);
             cellRef.appendChild(artistRef);
             cellRef.appendChild(nameRef);
+			//cellRef.appendChild(previewRef);
 
             cellRef.addEventListener('click', ()=>{
                 document.querySelector('.content').style.display = 'block';
 				container.style.display = 'none';
 				closeBtn.style.display = 'block';
-				detailedSongInfo(songName, artistName);
+				detailedSongInfo(infoData);
             })
 
         };
@@ -92,17 +131,28 @@ function search(query, container){
 
     };
 
-	function detailedSongInfo(songName, artistName){
-		let contentRef= document.querySelector('.content');
+	function detailedSongInfo(infoData){
+		
+		let contentRef = document.querySelector('.content');
 		let artistNameRef = document.createElement('h1');
-		let songNameRef = document.createElement('h2');
-
-		artistNameRef.textContent = songName;
-		songNameRef.textContent = artistName;
-
+		let songNameRef = document.createElement('h3');
+		let previewSongRef = document.createElement('div');
+		let imageCoverRef = document.createElement('img');
+		
+		artistNameRef.textContent = infoData.artistname;
 		contentRef.appendChild(artistNameRef);
+		
+		songNameRef.textContent = infoData.songname;
 		contentRef.appendChild(songNameRef);
+		
+		imageCoverRef.setAttribute('src', infoData.imagecover);
+		contentRef.appendChild(imageCoverRef);
+		
+		previewSongRef.innerHTML = "<iframe src=" + infoData.previewurl + "></iframe>";
+		contentRef.appendChild(previewSongRef);
+		
 
+		
 	}
 
 	function closePanel(){
